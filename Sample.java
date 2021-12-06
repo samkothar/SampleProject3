3cd041c6db69704757252a5d09564ca936ec9d3a
samkothar <samkothari1810@gmail.com>
2021-12-06 13:45:28 +0530

    New welcome message

--- Sample.java
+++ Sample.java
@@ -2,6 +2,7 @@ public class Sample {
     
      public static void main(String[] args) {
         
-        System.out.println("Hello and Welcome to Capgemini!!");
+        System.out.println("Hello");
+        System.out.println("Welcome to Capgemini");
     }
 }
